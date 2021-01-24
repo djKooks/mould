bb8b6b1e322f9b5be3b9c01917f3e8496488fa01
djKooks <inylove82@gmail.com>
2021-01-24 17:48:43 +0900

    basic config for test

---
+++ tests/mould.test.js
@@ -0,0 +1,26 @@
+const mould = require('../lib/mould')
+
+
+test('test function to check test module', () => {
+  expect(mould.getResult(2)).toBe(4)
+});
+
+
+
+
+test('test whether sheet exists', () => {
+
+})
+
+test('test whether sheet has value', () => {
+
+})
+
+test('test file has been generated', () => {
+
+})
+
+test('test value in file are same with sheet', () => {
+  
+})
+
